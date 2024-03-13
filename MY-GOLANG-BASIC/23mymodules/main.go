1e3642282cedeb324dcc6793f815a0e4b2c7f5c2
Vivek Kumar <118689020+vickvey@users.noreply.github.com>
2024-03-13 07:42:37 +0000

    added lec23

---
+++ MY-GOLANG-BASIC/23mymodules/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("Hello mod in goland")
+	greeter()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+
+func greeter() {
+	fmt.Println("Hey there mod users")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to golang series on YT</h1>"))
+}
